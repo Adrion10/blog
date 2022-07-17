0adc3dc0daf84d03fd69e39774c6832c2a54eb1a
Adrion10 <lacehila@gmail.com>
2022-07-17 22:56:32 +0200

    styling inline

--- src/app/user1-list/user1-list.component.ts
+++ src/app/user1-list/user1-list.component.ts
@@ -4,13 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-user1-list',
   templateUrl: './user1-list.component.html',
   styles: [
-  ]
+    `
+      .costem {
+        color: red;
+      }
+    `,
+  ],
 })
 export class User1ListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
